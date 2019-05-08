6be155bcc83cbec5a114af9a8da766cb81f11137
yingying095 <450023747@qq.com>
2019-05-08 10:25:43 +0800

    修改了一次

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        //  target: 'https://mock.boxuegu.com/mock/254',
-         target: 'http://mock.course.itcast.cn/mock/20',
+          target: 'https://mock.boxuegu.com/mock/254',
+        //  target: 'http://mock.course.itcast.cn/mock/20',
           //  target: ' http://hmapi.course.itcast.cn',
         // target: 'https://www.easy-mock.com/mock/5ab213e33666166110a94928/admin',
         // target: 'http://127.0.0.1:7001',
